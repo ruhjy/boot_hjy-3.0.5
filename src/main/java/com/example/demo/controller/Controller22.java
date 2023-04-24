352a8566502647343cd7bc65f2df31bd4b184859
ruhjy <108906280+ruhjy@users.noreply.github.com>
2023-04-24 09:09:51 +0900

    MyBatis 사용 - insert

--- src/main/java/com/example/demo/controller/Controller22.java
+++ src/main/java/com/example/demo/controller/Controller22.java
@@ -87,4 +87,43 @@ public class Controller22 {
 		int cnt = mapper.sql5(dto1, dto2);
 		log.info("{} row(s) affected", cnt);
 	}
+	
+	@RequestMapping("/link9")
+	public void method9() {
+		Dto09 dto1 = new Dto09(1000, "spring", 50.0);
+		Dto10 dto2 = new Dto10(2000, "spring boot", 20.0);
+		int cnt = mapper.sql6(dto1, dto2);
+		log.info("{} row(s) affected", cnt);
+	}
+	
+	// 경로 : /sub22/link10?property1=100&property2=memberA&property3=12.34&age=200&name=memberB&score=56.78
+	@RequestMapping("/link10")
+	public void method10(@ModelAttribute Dto09 p1, @ModelAttribute Dto10 p2) {
+		int cnt = mapper.sql6(p1, p2);
+		log.info("{} row(s) affected", cnt);
+	}
+
+	// 경로 : /sub22/link11?prop2=mybatis&prop3=321
+	@RequestMapping("/link11")
+	@ResponseBody
+	public String method11(@ModelAttribute Dto11 dto) {
+		log.info("prop1: {}", dto.getProp1());
+		int cnt = mapper.sql7(dto);
+		log.info("{}번 : {} row(s) affected", dto.getProp1(), cnt);
+		
+		return dto.getProp1() + "번째 데이터 입력 완료!!!";
+	}
+	
+	// 경로 : /sub22/link12?age=40&name=son&score=9.9
+	// 요청시 MyTable40에 데이터 추가되도록 코드
+	// 컨트롤러메서드, 매퍼 메서드, 자바빈
+	// *자바빈의 id 프로퍼티에 자동증가 컬럼값 받을 수 있게...
+	@RequestMapping("/link12")
+	@ResponseBody
+	public String method12(@ModelAttribute Dto12 param) {
+		int cnt = mapper.sql8(param);
+		log.info("id = {}, {} row(s) affected", param.getId(), cnt);
+		return param.getId() + " 번째 데이터 입력 완료!";
+	}
+	
 }
--- src/main/java/com/example/demo/mapper/Mapper03.java
+++ src/main/java/com/example/demo/mapper/Mapper03.java
@@ -9,7 +9,7 @@ public interface Mapper03 {
 
 	@Insert("insert into MyTable34 (Col1, Col2) values (#{val1}, #{val2})")
 	int sql1(Integer val1, String val2);
-	
+
 	// MyTable35 만들기
 	// controller22.method2에서 mapper03.sql2 메서드 호출
 	@Insert("insert into MyTable35 (Col1, Col2) values (#{v1}, #{v2})")
@@ -17,21 +17,45 @@ public interface Mapper03 {
 
 	/**
 	 * 파라미터 1개일 때
-	 * 프로퍼티명만 작성 
-	 * @Insert("insert into MyTable36 (Col1, Col2, Col3) values (#{property1}, #{property2}, #{property3})")
+	 * 프로퍼티명만 작성
+	 * @Insert("insert into MyTable36 (Col1, Col2, Col3) values (#{property1},
+	 * #{property2}, #{property3})")
 	 * int sql3(Dto09 dto);
 	 */
 	@Insert("insert into MyTable36 (Col1, Col2, Col3) values (#{dto.property1}, #{dto.property2}, #{dto.property3})")
 	int sql3(@Param("dto") Dto09 dto);
-	
+
 	// 1) MyTable37 테이블 만들기
 	// 2) Dto10 자바빈 만들기
 	// 3) mapper03.sql4 메서드를 controller22.method5에서 호출
 	@Insert("insert into MyTable37 (Age, Name, Score) values (#{age}, #{name}, #{score})")
 	int sql4(Dto10 dto);
-	
+
 	@Insert("insert into MyTable37 (Age, Name, Score) "
 			+ "values(#{dto9.property1}, #{dto10.name}, #{dto9.property3})")
 	int sql5(Dto09 dto9, Dto10 dto10);
+
+	/*
+	 * dto1.property1 -> col1
+	 * dto2.age -> col2
+	 * dto1.property2 -> col3
+	 * dto2.name -> col4
+	 * dto1.property3 -> col5
+	 * dto2.score -> col6
+	 * ? 부분 코드 수정
+	 * 이 메소드를 controller22.method9에서 호출
+	 */
+	@Insert("insert into MyTable38 (Col1, Col2, Col3, Col4, Col5, Col6) "
+			+ "values (#{dto1.property1}, #{dto2.age}, #{dto1.property2}, #{dto2.name}, #{dto1.property3}, #{dto2.score})")
+	int sql6(Dto09 dto1, Dto10 dto2);
 	
+	
+	@Insert("insert into MyTable39 (Col2, Col3) values (#{prop2}, #{prop3})")
+	@Options(useGeneratedKeys = true, keyProperty = "prop1")
+	int sql7(Dto11 dto);
+	
+	@Insert("insert into MyTable40 (age, name, score) values (#{age}, #{name}, #{score})")
+	@Options(useGeneratedKeys = true, keyProperty = "id")
+	int sql8(Dto12 dto);
+
 }
