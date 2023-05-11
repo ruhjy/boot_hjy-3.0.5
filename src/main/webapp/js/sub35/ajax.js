7e41b80a133d45a5bf48ddefaed0e50749b02895
ruhjy <108906280+ruhjy@users.noreply.github.com>
2023-05-11 17:47:52 +0900

    JSON.stringify() 사용해서 객체를 json으로 변환

--- src/main/webapp/js/sub35/ajax.js
+++ src/main/webapp/js/sub35/ajax.js
@@ -37,50 +37,121 @@ $("#button6").click(function() {
 
 /* json */
 $("#button7").click(function() {
+	const obj = { name: "son" };
+
 	$.ajax("/sub35/link7", {
 		method: "post",
-		data: '{"name": "son"}',
+		// data: '{"name": "son"}',
+		data: JSON.stringify(obj),
 		contentType: "application/json"
 	})
 });
 
 $("#button8").click(function() {
+	const data = {
+		address: "korea",
+		name: "홍길동"
+	};
+
 	$.ajax("/sub35/link8", {
 		method: "post",
-		data: '{"address": "korea", "name": "홍길동"}',
+		// data: '{"address": "korea", "name": "홍길동"}',
+		data: JSON.stringify(data),
 		contentType: "application/json"
 	})
 });
 
 $("#button9").click(function() {
+	const data = {
+		name: "hjy",
+		age: 33,
+		married: false
+	};
+
 	$.ajax("/sub35/link9", {
 		method: "post",
 		contentType: "application/json",
-		data: '{"name":"hjy", "age":33, "married":false}'
+		// data: '{"name":"hjy", "age":33, "married":false}'
+		data: JSON.stringify(data)
 	})
 });
 
 $("#button10").click(function() {
+	const obj = {
+		address: null,
+		age: 30,
+		name: "손흥민"
+	};
+
 	$.ajax("/sub35/link10", {
 		method: "post",
 		contentType: "application/json",
-		data: '{"address":null, "age":30, "name":"손흥민"}'
+		data: JSON.stringify(obj)
 	})
 });
 
 $("#button11").click(function() {
+	const obj = {
+		book: {
+			title: "java",
+			price: 300
+		},
+		address: "seoul"
+	};
+
 	$.ajax("/sub35/link11", {
 		method: "post",
 		contentType: "application/json",
-		data: '{"book":{"title":"java", "price":300}, "address":"seoul"}'
+		data: JSON.stringify(obj)
 	})
 });
 
 $("#button12").click(function() {
+	const obj = {
+		food: [
+			"pizza",
+			"coke",
+			"coffee"
+		],
+		store: "피자스쿨"
+	};
+
 	$.ajax("/sub35/link12", {
 		method: "post",
 		contentType: "application/json",
-		data: '{"food":["pizza", "coke", "coffee"], "store":"피자스쿨"}'
+		data: JSON.stringify(obj)
 	})
 });
 
+$("#button13").click(function() {
+	const obj = ["pizza", "coke", "coffee"];
+
+	$.ajax("/sub35/link13", {
+		method: "post",
+		contentType: "application/json",
+		data: JSON.stringify(obj)
+	})
+});
+
+$("#button14").click(function() {
+	const obj = [
+		{
+			name: "강백호",
+			position: "파워포워드"
+		},
+		{
+			name: "서태웅",
+			position: "스몰포워드"
+		},
+		{
+			name: "송태섭",
+			position: "포인트가드"
+		}
+	];
+
+	$.ajax("/sub35/link14", {
+		method: "post",
+		contentType: "application/json",
+		data: JSON.stringify(obj)
+	})
+});
